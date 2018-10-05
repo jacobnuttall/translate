c79136d46b6251771924329009975bebfc2fc33e
EC2 Default User <jacob_nuttall@outlook.com>
2018-10-05 06:13:59 +0000

    Can get list

--- script.js
+++ script.js
@@ -5,6 +5,45 @@
 jQuery(document).ready(function() {
     console.log("Document ready.");
     
+    // Messages for JSON requests
+    var jsons = "getJSON successful! ";
+    var jsonf = "getJSON failed. ";
+    var jsone = "getJSON ended. ";
+    
+    // URL for Yandex.Translate
+    var base_url = "https://translate.yandex.net/api/v1.5/tr.json/";
+    var key = "key=trnsl.1.1.20181004T224629Z.f8631add42092d38.b19e24472124c0e96edbe38fa0debba3205bd437";
+    var lang_list;
+    
+    // Get the list of languages
+    jQuery.getJSON( (base_url+"getLangs?"+key+"&ui=en"), function(data) {
+        // Check access to JSON
+        console.log(data);
+        console.log(data.langs);
+        
+        for (var i; i < data.langs.length; ++i) {
+            console.log(data.langs[i]);
+        }
+        
+                
+    })
+    
+    .done(function() {
+        console.log(jsons + "(getLangs) ");
+    })
+    
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log(jsonf + textStatus + "(getLangs)");
+    })
+    
+    .always(function() {
+        console.log(jsone + "(getLangs)");
+    });
+        
+    
+    
+    /*
+    
     $( "#submitButton" ).click(function(e) {
         var value = $("#translate").val();
         var value2 = $("#language").val();
@@ -15,6 +54,13 @@ jQuery(document).ready(function() {
         myurl += "&lang=en-";
         myurl += value2;
         console.log(myurl);
+        
+
+
+
+
+        
+        
         $.ajax({
         url : myurl,
         dataType : "json",
@@ -22,8 +68,13 @@ jQuery(document).ready(function() {
             var outCome = parsed_json['text'];
             $("#result").html(outCome);
             }
+            
+            
+            
         });
     });
+    
+    */
 });
 
 
