07c3e113bef5605695b329f97d18d07ca62f207e
EC2 Default User <jacob_nuttall@outlook.com>
2018-10-05 04:47:22 +0000

    Modified layout

---
+++ script.js
@@ -0,0 +1,22 @@
+/* global jQuery */
+/* global $ */
+
+$( "#submitButton" ).click(function(e) {
+    var value = $("#translate").val();
+    var value2 = $("#language").val();
+    console.log(value);
+    e.preventDefault();
+    var myurl= "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181004T224629Z.f8631add42092d38.b19e24472124c0e96edbe38fa0debba3205bd437&text=";
+    myurl += value;
+    myurl += "&lang=en-";
+    myurl += value2;
+    console.log(myurl);
+    $.ajax({
+    url : myurl,
+    dataType : "json",
+    success : function(parsed_json) {
+    var outCome = parsed_json['text'];
+    $("#result").html(outCome);
+    }
+  });
+});
